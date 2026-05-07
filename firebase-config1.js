b8c58484e69d1c9f9caf5b7564bf9b28dfecbb58
tym95959 <tym95959@gmail.com>
2026-05-07 13:57:59 +0500

    Create firebase-config1.js

---
+++ firebase-config1.js
@@ -0,0 +1,17 @@
+// Firebase Configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD6_tJpSN2ca8L12Rm9ulaosTJfG2SKUK8",
+    authDomain: "kpsyst-a5377.firebaseapp.com",
+    projectId: "kpsyst-a5377",
+    storageBucket: "kpsyst-a5377.firebasestorage.app",
+    messagingSenderId: "738297113997",
+    appId: "1:738297113997:web:1dd012a94ad77c981461c4",
+    measurementId: "G-H60ZMZ6LV6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// Collection name for maintenance reports
+const COLLECTION_NAME = "maintenance_reports";
