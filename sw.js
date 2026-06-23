9501f59dd21bee383a4a8022ae68ff8029dd3ccc
tym95959 <tym95959@gmail.com>
2026-06-23 15:58:57 +0500

    Update cached HTML pages in sw.js
    Replaced 'checklist.html' with 'CL2.html' in the service worker cache.

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const ASSETS_TO_CACHE = [
   'index.html',
   'manifest.json',
   // Add your other HTML pages here if they exist
-  // 'checklist.html',
+   'CL2.html',
   // 'shower.html',
   // 'briefing.html',
   // 'handover.html',
